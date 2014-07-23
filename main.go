a484bf90286683daa385768b702e19258cba2073
daimatz <dai@daimatz.net>
2014-07-24 04:04:24 +0900

    add Directory handle

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"path"
 
 	"github.com/daimatz/gomiki/html"
 )
@@ -15,14 +18,14 @@ const Addr string = ":8080"
 
 type RootHandler struct{}
 
-func (h RootHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (h *RootHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	var u *url.URL = req.URL
-	var path string = os.ExpandEnv(Home) + u.Path
+	var localPath string = os.ExpandEnv(Home) + u.Path
 
-	file, err := os.Open(path)
+	file, err := os.Open(localPath)
 	defer file.Close()
 	if err != nil {
-		log.Printf("failed to open %s\n", path)
+		log.Printf("failed to open %s\n", localPath)
 		if os.IsNotExist(err) {
 			http.Error(w, err.Error(), http.StatusNotFound)
 		} else {
@@ -31,15 +34,45 @@ func (h RootHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	md, err := ioutil.ReadAll(file)
+	stat, err := file.Stat()
 	if err != nil {
-		log.Printf("failed to read %s\n", path)
+		log.Printf("failed to get info %s\n", localPath)
+		http.Error(w, err.Error(), http.StatusForbidden)
+		return
+	}
+	if stat.IsDir() {
+		h.ServeDirectory(localPath, w)
+	} else {
+		path.Ext(localPath)
+		md, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Printf("failed to read %s\n", localPath)
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
+		}
+		h.WriteResponse(md, w)
+	}
+}
+
+func (h *RootHandler) ServeDirectory(localPath string, w http.ResponseWriter) {
+	var buf bytes.Buffer
+
+	infos, err := ioutil.ReadDir(localPath)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusForbidden)
 		return
 	}
 
-	err = html.MainTemplate.Execute(w, html.MainArg{
-		html.MarkdownToTemplateHTML(md),
+	for _, info := range infos {
+		buf.WriteString(fmt.Sprintf("- [%s](%s)\n", info.Name, info.Name))
+	}
+
+	h.WriteResponse(buf.Bytes(), w)
+}
+
+func (h *RootHandler) WriteResponse(content []byte, w http.ResponseWriter) {
+	err := html.MainTemplate.Execute(w, html.MainArg{
+		html.MarkdownToTemplateHTML(content),
 	})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -50,7 +83,7 @@ func (h RootHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 func main() {
 	server := &http.Server{
 		Addr:    Addr,
-		Handler: RootHandler{},
+		Handler: &RootHandler{},
 	}
 	log.Fatal(server.ListenAndServe())
 }
